b5ea3e590454ac9070f7340d9307a4e987f9565e
Erik Strottmann <erik.strottmann@gmail.com>
2017-11-12 22:53:44 -0800

    Initial commit

---
+++ NightShade/NightShadeView.h
@@ -0,0 +1,13 @@
+//
+//  NightShadeView.h
+//  NightShade
+//
+//  Created by Erik Strottmann on 11/12/17.
+//  Copyright © 2017 Erik Strottmann. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface NightShadeView : ScreenSaverView
+
+@end
---
+++ NightShade/NightShadeView.m
@@ -0,0 +1,52 @@
+//
+//  NightShadeView.m
+//  NightShade
+//
+//  Created by Erik Strottmann on 11/12/17.
+//  Copyright © 2017 Erik Strottmann. All rights reserved.
+//
+
+#import "NightShadeView.h"
+
+@implementation NightShadeView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
